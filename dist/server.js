f35546b5f012c9923c98b15535334d3102ae5338
Ronaldo Barbosa <60077368+ronaldornd@users.noreply.github.com>
2023-08-19 23:26:31 -0300

    fixes

--- dist/server.js
+++ dist/server.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
-const routes_1 = require("./routes");
+const routes_1 = require("./routes.ts");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
